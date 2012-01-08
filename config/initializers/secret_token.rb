c2382f97c9a5ea781e45795edf674a3c2bd5a820
Arman Dezfuli-Arjomandi <rawrmaan@gmail.com>
2012-01-07 16:20:51 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freefood::Application.config.secret_token = 'ef338102e71da648a95cbe55a4f65b33b4ab333aa3c789cd4b20cb2ed65f60a0b6eeb3dd84d7db37ae492589eb28093228ebadd19be7e98e9d9ea6dc9bef4eb2'
